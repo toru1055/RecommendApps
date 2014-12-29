5b366d5393378ec4dcc7dd9bb2e2d69415539fa6
toru1055 <toru1055h@gmail.com>
2014-12-29 21:48:32 +0900

    Remove setting button

--- app/src/main/java/jp/thotta/android/recommendapps/NoShowListActivity.java
+++ app/src/main/java/jp/thotta/android/recommendapps/NoShowListActivity.java
@@ -65,7 +65,7 @@ public class NoShowListActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_no_show_list, menu);
+        //getMenuInflater().inflate(R.menu.menu_no_show_list, menu);
         return true;
     }
 
--- app/src/main/java/jp/thotta/android/recommendapps/SettingActivity.java
+++ app/src/main/java/jp/thotta/android/recommendapps/SettingActivity.java
@@ -75,7 +75,7 @@ public class SettingActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        //getMenuInflater().inflate(R.menu.menu_setting, menu);
         return true;
     }
 
